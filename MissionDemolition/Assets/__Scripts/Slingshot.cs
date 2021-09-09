2b3f28fa046dac92b5d0b0dee975d73f1e0f26f7
DiegoHR <diego.hdez@hotmail.com>
2021-09-08 19:03:23 -0500

    Follow Cam & Projectile shot

--- MissionDemolition/Assets/__Scripts/Slingshot.cs
+++ MissionDemolition/Assets/__Scripts/Slingshot.cs
@@ -6,7 +6,8 @@ public class Slingshot : MonoBehaviour
 {
 
     [Header("Set In Inspector")]
-    public GameObject prefabProjectile;
+    public GameObject   prefabProjectile;
+    public float        velocityMult = 8f;
 
     [Header("Set Dynamically")]
     public GameObject   launchPoint;
@@ -14,6 +15,7 @@ public class Slingshot : MonoBehaviour
     public Vector3      launchPos;
     public GameObject   projectile;
     public bool         aimingMode;
+    private Rigidbody   projectileRigidBody;
 
     private void Awake()
     {
@@ -42,6 +44,44 @@ public class Slingshot : MonoBehaviour
 
         projectile.transform.position = launchPos;
 
-        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidBody = projectile.GetComponent<Rigidbody>();
+        projectileRigidBody.isKinematic = true;
+
+    }
+
+    public void Update()
+    {
+        if (!aimingMode)
+        { 
+            return; 
+        }
+
+        //obtener la posicion del raton en 2D
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //obtener la diferencia de launchPos a mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //limit mouseDelta to the radius of the slingshot Sphere Collider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude) 
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        //movemos el proyectil a esa nueva pose
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        //leemos si se solto el mouse
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            aimingMode = false;
+            projectileRigidBody.isKinematic = false;
+            projectileRigidBody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
     }
 }
